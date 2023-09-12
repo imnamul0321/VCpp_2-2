cb81715592dae12fc81319e55c3374c992687e28
unknown <sjlim0321@naver.com>
2023-09-12 23:02:07 +0900

    수정중

--- week2_1/project1/Project1/소스.cpp
+++ week2_1/project1/Project1/소스.cpp
@@ -5,43 +5,37 @@ using namespace std;
 
 int main() {
 
+
 	printf("202207010\n");
 	printf("임성진님의\n");
 	printf("Application\n");
 	printf("\n");
-	//char str[17] = "202207010 임성진";
-	//cout << str;
-	//printf("%s", str);
-	char esc;
 	while (1)
-	{
-		esc = _getch();
+	{	
+		
+		printf("command>");
+		char pfj;
+		pfj = _getch();
+		if (pfj == 27)
+			break;
+
+		if (pfj == 112)
 		{
-			if (int(esc) == 27)
-				break;
+			printf("\n나는 과제를 겁나 잘하는 학생이다.\n");
 		}
-		string pfj;
-		cout << "p, j, f 입력 : " << endl;
-		cin >> pfj;
-		if (pfj == "p")
-		{
-			printf("나는 과제를 겁나 잘하는 학생이다.\n");
+		else if (pfj == 102) {
+			printf("\n나는 과제가 너무 즐겁다.\n");
 		}
-		else if (pfj == "f") {
-			printf("나는 과제가 너무 즐겁다.\n");
-		}
-		else if (pfj == "j") {
-			printf("교수님 과제 더 내주세요.\n");
-		}
-		else if (pfj == "j") {
-			printf("교수님 과제 더 내주세요.\n");
+		else if (pfj == 106) {
+			printf("\n교수님 과제 더 내주세요.\n");
 		}
 
 		else {
-			printf("다시입력하세요.\n");
+			printf("\n다시입력하세요.\n");
 		}
 
-
 	}
+
 	return 0;
+
 }
