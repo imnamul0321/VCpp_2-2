c06cdc6e4bc856ac7912dac58a0334ecdf206e09
unknown <sjlim0321@naver.com>
2023-09-17 17:14:48 +0900

    2주차

--- week2_1/project1/Project1/소스.cpp
+++ week2_1/project1/Project1/소스.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
-#include <string>
-#include <conio.h>
+#include <cctype>   // isalpha 알파벳 확인함수
+#include <cstdio>   // getchar 아스키 코드로 한 문자 입력받는 함수
 using namespace std;
 
 int main() {
+	// 스플래쉬 이미지 출력
+	cout << " 202207010 " << endl;
+	cout << " 임성진" << endl;
+	cout << " VC++" << endl;
 
+	while (1) {
+		// 커맨드 입력받기
+		cout << "command> ";
+		char command = getchar(); //getchar 인가 _getch 인가 잘 모르겠다
 
-	printf("202207010\n");
-	printf("임성진님의\n");
-	printf("Application\n");
-	printf("\n");
-	while (1)
-	{	
-		
-		printf("command>");
-		char pfj;
-		pfj = _getch();
-		if (pfj == 27)
+		// 엔터 키 입력 처리
+		if (command == '\n') {
+			continue;
+		}
+
+		// q, w, e, r 커맨드 처리
+		if (command == 'q') {
+			cout << "qwer 화이팅" << endl;
+		}
+		else if (command == 'w') {
+			cout << "과제 너무 좋다" << endl;
+		}
+		else if (command == 'e') {
+			cout << "담주부턴 과제량 3배다" << endl;
+		}
+		else if (command == 'r') {
+			cout << "행복합니다." << endl;
+		}
+		else if (command == 27) {  // 27은 ESC 키의 ASCII 코드
+			cout << "프로그램을 종료합니다." << endl;
 			break;
-
-		if (pfj == 112)
-		{
-			printf("\n나는 과제를 겁나 잘하는 학생이다.\n");
 		}
-		else if (pfj == 102) {
-			printf("\n나는 과제가 너무 즐겁다.\n");
-		}
-		else if (pfj == 106) {
-			printf("\n교수님 과제 더 내주세요.\n");
-		}
-
 		else {
-			printf("\n다시입력하세요.\n");
+			cout << "잘못된 입력입니다. 다시 시도하세요." << endl;
 		}
-
 	}
 
 	return 0;
-
 }
